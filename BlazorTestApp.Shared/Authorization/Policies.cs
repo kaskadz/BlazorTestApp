e0cdd5cf47d9b5e452252e9470e0d05270631c10
Kasper <kasper.kadzielawa@gmail.com>
2021-07-25 10:24:15 +0200

    Add new policies

--- BlazorTestApp.Shared/Authorization/Policies.cs
+++ BlazorTestApp.Shared/Authorization/Policies.cs
@@ -17,5 +17,29 @@ namespace BlazorTestApp.Shared.Authorization
             public static readonly NamedAuthorizationPolicy NamedPolicy = NamedAuthorizationPolicy.Create(
                 PolicyName, policy => policy.RequireRole("195d4a45-4de4-4600-89f3-72cae83e4d7b"));
         }
+
+        public static class Player
+        {
+            public const string PolicyName = "Player";
+
+            public static readonly NamedAuthorizationPolicy NamedPolicy = NamedAuthorizationPolicy.Create(
+                PolicyName, policy => policy.RequireRole("dfd2c19f-5ed9-4d74-b02c-1e13dd13602f"));
+        }
+
+        public static class Host
+        {
+            public const string PolicyName = "Host";
+
+            public static readonly NamedAuthorizationPolicy NamedPolicy = NamedAuthorizationPolicy.Create(
+                PolicyName, policy => policy.RequireRole("54ec2745-bc5d-4c3c-9b0b-c152f25828f0"));
+        }
+
+        public static class Editor
+        {
+            public const string PolicyName = "Editor";
+
+            public static readonly NamedAuthorizationPolicy NamedPolicy = NamedAuthorizationPolicy.Create(
+                PolicyName, policy => policy.RequireRole("90fb28fc-c328-41b9-a8b5-a1dc63c00679"));
+        }
     }
 }
